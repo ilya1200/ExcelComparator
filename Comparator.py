32c411a593be5c510a5c9afa0188005ea53608ea
ilya1200 <ilya1200@gmail.com>
2022-02-02 21:23:47 +0200

    add 3 phases comparator

--- Comparator.py
+++ Comparator.py
@@ -1,4 +1,48 @@
+from typing import Dict, Tuple, List
+
+from openpyxl.worksheet.worksheet import Worksheet
+from pandas import DataFrame
+import pandas as pd
+
+
 class Comparator:
 
-    def compare(self, left_table, right_table, mapper_table):
-        pass
+    def compare(self, path: str, left_table: Worksheet, right_table: Worksheet, mapper_table: Worksheet):
+        left: DataFrame = pd.read_excel(path, left_table.title)
+        right: DataFrame = pd.read_excel(path, right_table.title)
+        mapping: DataFrame = pd.read_excel(path, mapper_table.title)
+
+        mapper_list: List[Tuple[str, str]] = list()
+
+        # Build mapper list
+        for i in range(len(mapping)):
+            mapper_list.append((mapping.values[i][0], mapping.values[i][1]))
+
+        # Compare columns by row
+        for l, r in mapper_list:
+            result = pd.DataFrame([left[l].isin([right[r]])]).T
+
+            # Report to xlsx
+            output = pd.ExcelWriter(f"Compare_{l}_{r}.xlsx")
+            result.to_excel(output)
+            output.save()
+
+        # Compare by unique values
+        for l, r in mapper_list:
+            lu = left[l].unique().T
+            ru = right[r].unique().T
+            rl = pd.Series(lu, name=str([left[l]])).array
+            rr = pd.Series(ru, name=str([right[r]])).array
+
+            # Swap to make sure rr is the longest
+            if len(rr) < len(rl):
+                temp = rr
+                rr = rl
+                rl = temp
+
+            result = pd.DataFrame([rr.T, rr.isin(rl).T]).T
+
+            # Report to xlsx
+            output = pd.ExcelWriter(f"Unique {l}.xlsx")
+            result.to_excel(output)
+            output.save()
--- main.py
+++ main.py
@@ -2,11 +2,9 @@ from typing import List
 
 from openpyxl import Workbook
 from openpyxl.worksheet.worksheet import Worksheet
-from pandas import DataFrame
 
 from Comparator import Comparator
 from ExcelHandler import ExcelHandler
-import pandas as pd
 
 if __name__ == '__main__':
     PATH_TO_EXCEL: str = "assignment.xlsx"
@@ -19,7 +17,4 @@ if __name__ == '__main__':
     scalable_sheet: Worksheet = worksheets[1]
     mapping_sheet: Worksheet = worksheets[2]
 
-    legacy: DataFrame = pd.read_excel(PATH_TO_EXCEL, legacy_sheet.title)
-    scalable: DataFrame = pd.read_excel(PATH_TO_EXCEL, scalable_sheet.title)
-    mapping: DataFrame = pd.read_excel(PATH_TO_EXCEL, mapping_sheet.title)
-    print()
+    comparator.compare(PATH_TO_EXCEL, legacy_sheet, scalable_sheet, mapping_sheet)
